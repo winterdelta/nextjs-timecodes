c781925297b8469aaf340d331229fc22beca0ad1
winterdelta <w@winterdelta.com>
2022-06-23 15:57:54 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nextjs-nycxaz
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/nextjs-nycxaz)
---
+++ pages/index.js
@@ -0,0 +1,33 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import Timecode from 'react-timecode';
+import { useState, useEffect } from 'react';
+
+export default function Home() {
+  const [date, setDate] = useState(new Date());
+
+  useEffect(() => {
+    let timerID = setInterval(() => tick(), 1000);
+
+    return function cleanUp() {
+      clearInterval(timerID);
+    };
+  });
+
+  const tick = () => {
+    setDate(new Date());
+  };
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+      </Head>
+
+      <main className={styles.main}>
+        <div>This is the time:</div>
+        <div>{date.toLocaleTimeString()}</div>
+      </main>
+    </div>
+  );
+}
